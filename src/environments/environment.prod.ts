273c7b3b73c094daff591f78c760ea17a47d2ee1
Elana Olson <elanaolson@gmail.com>
2019-07-19 16:18:28 -0700

    allow firebase on github

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB8J33uMPTZ1-UsBC9fAOmjDTdqWKUaFfY',
+    authDomain: 'ngecosystem-3ac34.firebaseapp.com',
+    databaseURL: 'https://ngecosystem-3ac34.firebaseio.com',
+    projectId: 'ngecosystem-3ac34',
+    storageBucket: 'ngecosystem-3ac34.appspot.com',
+    messagingSenderId: '1046980643403',
+    appId: '1:1046980643403:web:b27d9cbed73b23e1'
+  }
+};
